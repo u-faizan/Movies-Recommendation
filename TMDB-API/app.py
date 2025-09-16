d57b3ebcd6a2a2395cc63d3fa10b7ff968dd2f59
u-faizan <u2020cs94@student.comsats.edu.pk>
2025-09-16 23:35:29 +0500

    Add TFIDF-KNN project files, update TMDB-API, setup .gitignore

--- TMDB-API/app.py
+++ TMDB-API/app.py
@@ -15,7 +15,7 @@ IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 HEADERS = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwZWEyMTcyNTUzYWFkM2EwYTA3NjVmNGQ3NDYxYjE2MSIsIm5iZiI6MTc1Nzc1NjkxMS40MDIsInN1YiI6IjY4YzUzZGVmNzAyMWE5YjdiMGE4NjA1MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PuB2tOFuSgHF1oab9Ac7sE1FnVxsTsWMslIOw8zZum0"
+    "Authorization": ""  # replace with your key
 }
 
 def search_movie_tmdb(query):
